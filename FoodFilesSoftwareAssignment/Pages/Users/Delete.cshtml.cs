a645790b3f53fa8cb3dfcf671894a7b5cdb571ad
Hayden.L <1273728@studentmail.ucol.ac.nz>
2020-05-21 11:38:48 +1200

    Added User Database - Functional, but Incomplete.

---
+++ FoodFilesSoftwareAssignment/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FoodFilesSoftwareAssignment.Data;
+using FoodFilesSoftwareAssignment.Models;
+
+namespace FoodFilesSoftwareAssignment.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly FoodFilesSoftwareAssignment.Data.UserContext _context;
+
+        public DeleteModel(FoodFilesSoftwareAssignment.Data.UserContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FindAsync(id);
+
+            if (User != null)
+            {
+                _context.User.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
