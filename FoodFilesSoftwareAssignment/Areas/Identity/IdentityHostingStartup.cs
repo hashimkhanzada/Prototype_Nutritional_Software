5d6d03ba149bccaf5833db20af135c0542c52ad4
Hayden.L <1273728@studentmail.ucol.ac.nz>
2020-06-15 10:49:41 +1200

    Changes to password requirements for new accounts - min length is 5 characters with no specific character requirements.

--- FoodFilesSoftwareAssignment/Areas/Identity/IdentityHostingStartup.cs
+++ FoodFilesSoftwareAssignment/Areas/Identity/IdentityHostingStartup.cs
@@ -25,6 +25,9 @@ namespace FoodFilesSoftwareAssignment.Areas.Identity
                     options.SignIn.RequireConfirmedAccount = false;
                     options.Password.RequireLowercase = false;
                     options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 5;
                 })
                     .AddEntityFrameworkStores<FFSA_AuthDBContext>();
             });
